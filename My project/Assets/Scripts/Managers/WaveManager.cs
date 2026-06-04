417a92a574fff372d3942581ebe81a1707f15e04
OscarMaestre987 <oscarmaestregonzalez@gmail.com>
2026-06-04 07:53:03 +0200

    arreglar algunos cambios

--- My project/Assets/Scripts/Managers/WaveManager.cs
+++ My project/Assets/Scripts/Managers/WaveManager.cs
@@ -62,7 +62,7 @@ public class WaveManager : MonoBehaviour
 
         yield return StartNextWaveDelay();
 
-        delay = Mathf.Max(delay - 0.01f, 0.1f);
+        delay = Mathf.Max(delay - 0.01f, 0.01f);
 
         num = currentWave / 10;
         currentWavePoints = currentWavePoints + (int)(pointsPerWaveIncrease * Mathf.Pow(2, num));
